e687b636ee7e6e906b5e1f52c730204d7a236e57
NidhiG9 <nghorpade09092000@gmail.com>
2024-07-18 00:26:18 +0530

    Create variables.tf

---
+++ modules/rds/variables.tf
@@ -0,0 +1,44 @@
+variable "dbname" {
+  
+}
+
+variable "username" {
+  
+}
+
+variable "password" {
+  
+}
+
+variable "rds_availability_zone" {
+    type = string
+  
+}
+
+variable "rds_security_group_id" {
+  
+}
+
+variable "subnet_ids" {
+  
+}
+
+variable "publicly_accessible" {
+    type = bool
+  
+}
+
+variable "engine" {
+    type = string
+  
+}
+
+variable "engine_version" {
+    type = string
+  
+}
+
+variable "instance_class" {
+    type = string
+  
+}
